593f0c478e71d029f2b3d84c561b35ff2ebfd713
Abu Rahman <abobaker.rahman10@gmail.com>
2023-03-02 00:36:29 +0100

    DrawLSysteem werkt Hardcode

--- Functies.h
+++ Functies.h
@@ -30,6 +30,8 @@ public:
 
     static void tekenReplace(string initiator, double starting_angle, double angle, Lines2D &lines, double x, double y, vector<pair<char,string>> replacements, unsigned int iterations);
 
+    static void leesStringRecursie(string rep_rule,double starting_angle, double angle, Lines2D &lines, double x, double y, pair<char,string> let);
+
 };
 
 #endif //ENGINE_FUNCTIES_H
