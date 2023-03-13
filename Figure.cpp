c25079567973e352468af58c60cdf2749d08eef7
aburahman10 <abobaker.rahman10@gmail.com>
2023-03-13 19:20:25 +0100

    class Figure

---
+++ Figure.cpp
@@ -0,0 +1,5 @@
+//
+// Created by aburahman10 on 13/03/23.
+//
+
+#include "Figure.h"
---
+++ Figure.h
@@ -0,0 +1,22 @@
+//
+// Created by aburahman10 on 13/03/23.
+//
+
+#ifndef ENGINE_FIGURE_H
+#define ENGINE_FIGURE_H
+
+#include "iostream"
+#include "vector"
+#include "Point2D.h"
+#include "Colour.h"
+
+using namespace std;
+
+class Figure
+{
+    vector<Point2D> points;
+    Colour color;
+};
+
+
+#endif //ENGINE_FIGURE_H
